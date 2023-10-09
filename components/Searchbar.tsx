0c50a4419ed4440889dc6ecff06f24458ee78b11
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2023-10-09 22:08:42 +0530

    Dev. handleSubmit and changes in next.config

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,20 +1,71 @@
 "use client";
 
-const Searchbar = () => {
-  const handleSubmit = () => {};
+import { scrapeAndStoreProduct } from "@/lib/actions";
+import { FormEvent, useState } from "react";
+
+const isValidAmazonProductURL = (url: string) => {
+  try {
+    const parsedURL = new URL(url);
+    const hostname = parsedURL.hostname;
+
+    // Check if hostname is amazon.com or amazon.in
+    if (
+      hostname.includes("amazon.com") ||
+      hostname.includes("amazon.in") ||
+      hostname.includes("amazon.") ||
+      hostname.endsWith("amazon")
+    ) {
+      return true;
+    }
+  } catch (error: any) {
+    return false;
+  }
+};
+
+const SearchBar = () => {
+  const [searchPrompt, setSearchPrompt] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const isValidLink = isValidAmazonProductURL(searchPrompt);
+
+    if (!isValidLink) return alert("Please enter a valid Amazon product link");
+
+    try {
+      // loading state true
+      setIsLoading(true);
+
+      // Scrape the product image
+      const product = await scrapeAndStoreProduct(searchPrompt);
+    } catch (error) {
+      // error handling
+      console.log(error);
+    } finally {
+      // loading state false
+      setIsLoading(false);
+    }
+  };
 
   return (
     <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
       <input
         type="text"
+        value={searchPrompt}
+        onChange={(e) => setSearchPrompt(e.target.value)}
         placeholder="Enter product link"
         className="searchbar-input"
       />
-      <button type="submit" className="searchbar-btn">
-        Search
+      <button
+        type="submit"
+        className="searchbar-btn"
+        disabled={searchPrompt === ""}
+      >
+        {isLoading ? "Searching..." : "Search"}
       </button>
     </form>
   );
 };
 
-export default Searchbar;
+export default SearchBar;
