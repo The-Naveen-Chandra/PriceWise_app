e424a20f2d249a22be1ddbd7e5f80eb183053ef3
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2023-10-10 16:15:37 +0530

    Extract the res with cheerio and utils fun

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
+import * as cheerio from "cheerio";
+import { extractCurrency, extractDescription, extractPrice } from "../utils";
 
 export async function scrapeAmazonProduct(url: string) {
   if (!url) return;
@@ -20,8 +22,72 @@ export async function scrapeAmazonProduct(url: string) {
   try {
     // Fetch the product page
     const response = await axios.get(url, options);
+    const $ = cheerio.load(response.data);
 
-    console.log(response.data);
+    // Extract the product title
+    const title = $("#productTitle").text().trim();
+
+    // Extract the product price
+    const currentPrice = extractPrice(
+      $(".priceToPay span.a-price-whole"),
+      $("a.size.base.a-color-price"),
+      $(".a-button-selected .a-color-base")
+      // $("a.price.a-text-price")
+    );
+
+    // Extract the original price
+    const originalPrice = extractPrice(
+      $("#priceblock_ourprice"),
+      $(".a-price.a-text-price span.a-offscreen"),
+      $("#listPrice"),
+      $("#priceblock_dealprice"),
+      $(".a-size-base.a-color-price")
+    );
+
+    // Check if the product is out of stock
+    const outOfStock =
+      $("#availability span").text().trim().toLowerCase() ===
+      "currently unavailable";
+
+    // Extract the product images
+    const images =
+      $("#imgBlkFront").attr("data-a-dynamic-image") ||
+      $("#landingImage").attr("data-a-dynamic-image") ||
+      "{}";
+
+    // Convert the images to an array of URLs
+    const imageUrls = Object.keys(JSON.parse(images));
+
+    // Extract the product currency
+    const currency = extractCurrency($(".a-price-symbol"));
+
+    // Extract the product discount rate
+    const discountRate = $(".savingsPercentage").text().replace(/[-%]/g, "");
+
+    // Extract the product description
+    const description = extractDescription($);
+
+    // construct data object with scraped information
+    const data = {
+      url,
+      currency: currency || "₹",
+      image: imageUrls[0],
+      title,
+      currentPrice: Number(currentPrice) || Number(originalPrice),
+      originalPrice: Number(originalPrice) || Number(currentPrice),
+      discountRate: Number(discountRate),
+      priceHistory: [],
+      category: "category",
+      reviewsCount: 100,
+      stars: 4.5,
+      isOutOfStock: outOfStock,
+      description,
+      lowestPrice: Number(currentPrice) || Number(originalPrice),
+      highestPrice: Number(originalPrice) || Number(currentPrice),
+      averagePrice: Number(currentPrice) || Number(originalPrice),
+    };
+
+    return data;
   } catch (error: any) {
     throw new Error(`Failed to scrape product: ${error.message}`);
   }
